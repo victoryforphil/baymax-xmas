adc33f9f3360e4367c6827cc444318fd44c65274
Alex Carter <victoryforphil@gmail.com>
2020-11-12 12:41:13 -0800

    Test TCP server

---
+++ controller/test.js
@@ -0,0 +1,34 @@
+const Net = require('net');
+// The port on which the server is listening.
+const port = 7777;
+const server = new Net.Server();
+
+server.listen(port, function() {
+    console.log(`Server listening for connection requests on socket localhost:${port}`);
+});
+
+// When a client requests a connection with the server, the server creates a new
+// socket dedicated tso that client.
+server.on('connection', function(socket) {
+    console.log('A new connection has been established.');
+
+    // Now that a TCP connection has been established, the server can send data to
+    // the client by writing to its socket.
+    socket.write('Hello, client.');
+
+    // The server can also receive data from the client by reading from its socket.
+    socket.on('data', function(chunk) {
+        console.log(`Data received from client: ${chunk.toString()}`);
+    });
+
+    // When the client requests to end the TCP connection with the server, the server
+    // ends the connection.
+    socket.on('end', function() {
+        console.log('Closing connection with the client');
+    });
+
+    // Don't forget to catch error, for your own sake.
+    socket.on('error', function(err) {
+        console.log(`Error: ${err}`);
+    });
+});
